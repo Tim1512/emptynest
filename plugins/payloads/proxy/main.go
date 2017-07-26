67ef2b04e1b029180ffd01665bde43c73ee9c322
Tom Steele <thomasjsteele@gmail.com>
2017-07-26 11:40:30 -0600

    Fix proxy code

--- plugins/payloads/proxy/main.go
+++ plugins/payloads/proxy/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
 // Name is a descriptive name of the
 func Name() string {
 	return "proxy"
@@ -10,12 +16,36 @@ func ID() int {
 	return 3
 }
 
+// String returns a friendly version of the payload.
+func String(data []byte) string {
+	return string(data)
+}
+
 // Help returns documentation for the plugin.
 func Help() string {
 	return "proxy <url>"
 }
 
-// Generate generates a payload based on the arguments provided
+// Generate generates a payload based on the arguments provided.
 func Generate(args []string) ([]byte, error) {
+	url := string(args[0])
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	return body, err
+}
 
+// Process prepares a payload based on the arguments provided.
+func Process(args []string) ([]byte, error) {
+	if len(args) < 1 {
+		return []byte{}, errors.New("missing required argument")
+	}
+	return []byte(args[0]), nil
 }
